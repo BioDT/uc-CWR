db3f3d79de2c3f0583f0ff3071f08f2b8bab3cc5
Erik Kusch <erik@i-solution.de>
2024-01-31 14:55:42 +0100

    BioDT API Credentials. Will be defunct by the end of BioDT

---
+++ R Scripts/SHARED-APICredentials.R
@@ -0,0 +1,7 @@
+API_User <- 284500
+API_Key <- "4b988f80-d86e-4d02-9eb0-0d5d0a72c6e5"
+numberOfCores <- parallel::detectCores()
+
+options(gbif_user = "biodt-cwr")
+options(gbif_email = "biodt-robot@gbif.no")
+options(gbif_pwd = "CWRpDT2024")
