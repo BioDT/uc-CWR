7d92b1be41dddcc043a1533438851b42ac35fad7
Tuomas Rossi <tuomas.rossi@csc.fi>
2024-02-02 09:32:01 +0200

    Check LUMI flag

--- R Scripts/SHARED-APICredentials.R
+++ R Scripts/SHARED-APICredentials.R
@@ -2,8 +2,12 @@ API_User <- 284500
 API_Key <- "4b988f80-d86e-4d02-9eb0-0d5d0a72c6e5"
 
 # Choose the number of parallel processes
-numberOfCores <- strtoi(Sys.getenv("SLURM_NTASKS"))
-if (is.na(numberOfCores)) {
+if (strtoi(Sys.getenv("CWR_ON_LUMI")) == 1) {
+    numberOfCores <- strtoi(Sys.getenv("SLURM_NTASKS"))
+    if (is.na(numberOfCores)) {
+        numberOfCores <- 1
+    }
+} else {
     numberOfCores <- parallel::detectCores()
 }
 
