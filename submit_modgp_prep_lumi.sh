4f2382611871aa4ca0ce3d6fba1ba0c42c215d2d
Tuomas Rossi <tuomas.rossi@csc.fi>
2025-01-10 13:52:42 +0200

    Update container version

--- submit_modgp_prep_lumi.sh
+++ submit_modgp_prep_lumi.sh
@@ -19,5 +19,5 @@ SPECIES="${1:-Lathyrus}"
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export CWR_ON_LUMI=1
 
-singularity run --bind $PWD cwr_0.4.1.sif "ModGP MASTER.R" "$SPECIES"
+singularity run --bind $PWD cwr_0.4.2.sif "ModGP MASTER.R" "$SPECIES"
 
