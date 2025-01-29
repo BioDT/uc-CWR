ca2a95150c65def0063481f5b483f2dbd1c3e10d
Tuomas Rossi <tuomas.rossi@csc.fi>
2025-01-29 12:30:27 +0200

    Use smaller resources

--- submit_modgp_prep_lumi.sh
+++ submit_modgp_prep_lumi.sh
@@ -4,15 +4,11 @@
 #SBATCH --account=project_465000915
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=128
-#SBATCH --time=04:00:00
-#SBATCH --partition=largemem
-##SBATCH --partition=lumid
-##SBATCH --partition=small --mem-per-cpu=2G
+#SBATCH --cpus-per-task=8
+#SBATCH --time=12:00:00
+#SBATCH --partition=small --mem=64G
 ##SBATCH --partition=standard --exclusive --mem=0
 ##SBATCH --partition=debug --exclusive --mem=0 --time=0:30:00
-
-# SDM calculation needs about 32 GB per task.
 
 SPECIES="${1:-Lathyrus}"
 
