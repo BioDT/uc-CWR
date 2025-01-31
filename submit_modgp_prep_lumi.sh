472dedee69786fad08775e051c43230fc6d9a26f
Michal Torma <michal.torma@nhm.uio.no>
2025-01-31 12:33:51 +0200

    Increase SLURM job time allocation to 24 hours

--- submit_modgp_prep_lumi.sh
+++ submit_modgp_prep_lumi.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --partition=small --mem=64G
 ##SBATCH --partition=standard --exclusive --mem=0
 ##SBATCH --partition=debug --exclusive --mem=0 --time=0:30:00
